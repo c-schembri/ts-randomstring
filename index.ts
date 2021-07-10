e4453bf4d3de77e2655c10d45d2d256f4e34a32c
Double <47208537+doubletruth@users.noreply.github.com>
2021-07-11 04:50:24 +1000

    update version

---
+++ index.ts
@@ -0,0 +1,12 @@
+export { 
+    RandomString as RandomString, 
+    generateRandomString as generateRandomString,
+    generateRandomStringAsync as generateRandomStringAsync
+} from "./src/randomString";
+
+export { 
+    Capitalisation as Capitalisation, 
+    Capitalization as Capitalization, 
+    CharacterSetType as CharacterSetType,
+    getCharacterSet as getCharacterSet
+} from "./src/characterSetHandler";
