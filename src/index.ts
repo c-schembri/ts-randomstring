ffa97015e7316106a1f8d5235e624941734c628d
Double <47208537+doubletruth@users.noreply.github.com>
2021-07-11 04:10:57 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import { RandomString as rs } from "./randomString";
+import { Capitalisation as cs, Capitalization as cz, CharacterSetType as cst, getCharacterSet as gct} from "./characterSetHandler";
+
+export const RandomString       = rs;
+export const Capitalisation     = cs;
+export const Capitalization     = cz;
+export const CharacterSetType   = cst;
+export const getCharacterSet    = gct;
